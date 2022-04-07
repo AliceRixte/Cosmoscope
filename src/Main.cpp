c0ad36aa5c6cbc20797ef3b7e9327f7d67b0547a
Diaseis <alice.rixte@gmail.com>
2022-04-07 14:50:09 +0200

    Clean up main file.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,10 @@
-#include <SDL.h>
 #include <iostream>
+
 #include "CSC_FuncTree.h"
-#include <cmath>
-#include "brushes.h"
-#include "CosmosToSDL.h"
-#include "CallbackExamples.h"
 #include "CosmosDrawer.h"
 
+#include "CallbackExamples.h"
 
-
-
-#include <filesystem>
-#include <string>
-
-namespace fs = std::filesystem;
-
-using std::filesystem::current_path;
 using namespace cosmoscope;
 
 int main(int argc, char* argv[])
@@ -23,23 +12,15 @@ int main(int argc, char* argv[])
     cosmoscope::FuncTree ftree{ cosmoscope::Position{450,450} };
     ftree.AddMonochromeFunc(-1, &line, cosmoscope::CoorSystem::Polar, cosmoscope::Style{ 1.,0.,0.,1.,-1. });
     ftree.AddPolychromeFunc(0, &circle, cosmoscope::CoorSystem::Polar, &style2);
-    ftree.AddMonochromeFunc(1, &circle4, cosmoscope::CoorSystem::Polar, cosmoscope::Style{ 0.9,0.1,0.2,1., });
+    ftree.AddMonochromeFunc(1, &circle4, cosmoscope::CoorSystem::Polar, cosmoscope::Style{ 0.9,0.1,0.2,1.,1.});
     // ftree.AddPolychromeFunc(1, &circle3, cosmoscope::CoorSystem::Polar, &style1);
      //ftree.AddPolychromeFunc(2, &circle3, cosmoscope::CoorSystem::Polar, &style2);
 
-
-
     cosmoscope_SDL::CosmosDrawer cosmosDrawer1{ "Cosmoscope",900,900,&ftree};
-    cosmoscope_SDL::CosmosDrawer cosmosDrawer2{ "Cosmoscope",900,900,&ftree };
 
-
-    while (cosmosDrawer1.IsWindowOpen()&& cosmosDrawer2.IsWindowOpen()) {
+    while (cosmosDrawer1.IsWindowOpen()) {
         cosmosDrawer1.ProcessEvents();
         cosmosDrawer1.UpdateFrame();
-
-        cosmosDrawer2.ProcessEvents();
-        cosmosDrawer2.UpdateFrame();
-
     }
 
     return 0;
