53e5d42566d7f9bf1287c706599fbcf1fd81c4a0
Diaseis <alice.rixte@gmail.com>
2022-04-13 11:20:20 +0200

    Generating circles (clean up)

--- include/CallbackExamples.h
+++ include/CallbackExamples.h
@@ -6,49 +6,12 @@
 #pragma once
 
 #include "CSC_CallbackFuncs.h"
+#include <cmath>
 
 #define TWOPI 6.28318530718
 
 using namespace cosmoscope;
 
-/* PolarPos line(Time t) {
-    return PolarPos{ 200,t/TWOPI};
-}
-
-PolarPos circle(Time t) {
-    return PolarPos{ 150,t / 150.0 /TWOPI };
-}
-PolarPos circle2(Time t) {
-    return PolarPos{ 60,t / 60 /TWOPI };
-}
-
-PolarPos circle3(Time t) {
-    return PolarPos{ 10,t / 2/TWOPI};
-}
-
-PolarPos circle4(Time t) {
-    return PolarPos{ 60,-t/TWOPI};
-}
-
-*/
-PolarPos line(Time t) {
-    return PolarPos{ 150,t/200.5 / TWOPI };
-}
-
-PolarPos circle(Time t) {
-    return PolarPos{ 105,t / 150.0 / TWOPI };
-}
-PolarPos circle2(Time t) {
-    return PolarPos{ 105,t / 85 / TWOPI };
-}
-
-PolarPos circle3(Time t) {
-    return PolarPos{ 10,t / 2 / TWOPI };
-}
-
-PolarPos circle4(Time t) {
-    return PolarPos{ 60,-t / TWOPI };
-}
 
 Style style1(Time t) {
     return Style{ 0,(static_cast<int>(t) % 20000) / 20000.0,0.8,1.,0.2 };
@@ -58,6 +21,6 @@ Style style2(Time t) {
 }
 
 Style style3(Time t) {
-    double coeff = (static_cast<int>(t) % static_cast<int> (42.5 * TWOPI)) / (42.5 * TWOPI);
+    double coeff = std::fmod(t,42.5 * TWOPI)/ (42.5 * TWOPI);
     return Style{ 0.00, coeff ,0.5 + 0.5*coeff,1.,0. };
 }
--- src/Main.cpp
+++ src/Main.cpp
@@ -10,15 +10,15 @@ using namespace cosmoscope;
 
 int main(int argc, char* argv[])
 {
-    cosmoscope::FuncTree ftree{ cosmoscope::CartesianPos{450,450} };
-    //ftree.AddMonochromeFunc(-1, &line   , cosmoscope::Style{ 1.,0.,0.,1.,-1. });
-    //ftree.AddPolychromeFunc(0,  &circle , &style2);
-    //ftree.AddPolychromeFunc(1, &circle4, &style1); //cosmoscope::Style{ 0.9,0.1,0.2,1.,0.0});
+    cosmoscope::FuncTree ftree{ cosmoscope::CartesianPos{495,495},450};
+    ftree.AddMonochromeFunc(-1, createCircle(200.0,1.0), cosmoscope::Style{1.,0.,0.,1.,-1.});
+    ftree.AddMonochromeFunc(0,  createCircle(150.0,1/150.0), cosmoscope::Style{1.,0.,0.,1.,-1.});
+    ftree.AddPolychromeFunc(1, createCircle(60.0,-1.0), &style1); //cosmoscope::Style{ 0.9,0.1,0.2,1.,0.0});
 
-    ftree.AddMonochromeFunc(-1, &line, cosmoscope::Style{ 1.,0.,0.,1.,-1. });
-    ftree.AddPolychromeFunc(0, createCircle(105,1/85.0), &style3);
+    //ftree.AddMonochromeFunc(-1, createCircle(300,1/401.0), cosmoscope::Style{1.,0.,0.,1.,-1.});
+    //ftree.AddPolychromeFunc(0, createCircle(190,1/170.0), &style3);
 
-    cosmoscope_SDL::CosmosDrawer cosmosDrawer1{ "Cosmoscope",900,900,&ftree};
+    cosmoscope_SDL::CosmosDrawer cosmosDrawer1{ "Cosmoscope",1000,1000,&ftree};
 
     while (cosmosDrawer1.IsWindowOpen()) {
         cosmosDrawer1.ProcessEvents();
